d513d1b0d6eb1eeaaa390cf68885f54e129b52e2
Social <68400635+leader1gnite@users.noreply.github.com>
2025-09-16 18:16:27 +0300

    sm8250-common: Take audio from vendor (test)

--- kona.mk
+++ kona.mk
@@ -102,12 +102,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    audio.primary.kona \
     audio.r_submix.default \
     audio.usbv2.default \
 
 PRODUCT_PACKAGES += \
-    liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
     libbundlewrapper \
